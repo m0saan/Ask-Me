18ad1a20bde5b21ed80fa9678dfd8d5fd756dd30
MoBoustta <moboustta6@gmail.com>
2020-11-04 20:47:59 +0100

    Initial Commit

---
+++ Ask Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ask Me
+//
+//  Created by moboustt on 11/4/20.
+//  Copyright © 2020 moboustt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
